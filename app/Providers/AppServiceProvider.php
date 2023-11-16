9fe8c53b607ed7000b2f032d602a57396235ff45
Volodymyr Voloshyn <mailvoloshyn@gmail.com>
2023-11-16 16:10:27 +0200

    third

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\File;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        parent::boot();
+
+        $imagesPath = public_path('storage/images');
+        File::ensureDirectoryExists($imagesPath);
     }
 }
